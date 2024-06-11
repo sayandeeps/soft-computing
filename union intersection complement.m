25aa9bfff867ea3b22c970fa22a5ed8c42029664
Sayandeep Sharma <89361557+sayandeeps@users.noreply.github.com>
2024-06-12 04:47:43 +0530

    Create union intersection complement.m

---
+++ union intersection complement.m
@@ -0,0 +1,19 @@
+%BASIC OPERATION LIKE UNION INTERSECTION  COMPLEMENT ETC
+u=input("Enter First Matrix");
+v=input('Enter Second Matrix');
+w=max(u,v);
+p=min(u,v);
+q1=1-u;
+q2=1-v;
+display('Union of Two Matrices');
+display(w);
+display('Intersection of Two Metrices');
+display(p);
+display('Complement of First Matix');
+display(q1);
+display('Compement of Second Matrix');
+display(q2);
+
+%OUTPUT
+%[0.3 0.4]
+%[0.1 0.7]
